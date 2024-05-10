a4eed38addb932d783e482324bf693bc711ef6f1
Carlos Ferranti <carlosferranti@gmail.com>
2024-05-10 16:11:31 -0300

    create and update.

---
+++ receiver/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "mensagem...",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine("Aguardando a mensagens");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    var aluno = JsonSerializer.Deserialize<Aluno>(message);
+
+    Console.WriteLine($" RECEDIBO: {message}");
+};
+
+channel.BasicConsume(
+    queue: "fila_mensagem",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine("Aperte a tecla <ENTER> para encerrar.");
+Console.ReadLine();
+
+class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataCriacao { get; }
+    public DateTime DataAtualizacao { get; set; }
+
+    public Aluno(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+        DataCriacao = DateTime.Now;
+        DataAtualizacao = DateTime.Now;
+    }
+}
---
+++ send/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "fila_mensagem",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine("Informe a mensagem a ser enviada e pressione a tecla <ENTER>");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == null || message.ToLower() == "exit")
+        break;
+
+    var aluno = new Aluno(1, "Bob");
+    message = JsonSerializer.Serialize(aluno);
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: "fila_mensagem",
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($"Foi enviado: {message}");
+}
+
+
+class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataCriacao { get; }
+    public DateTime DataAtualizacao { get; set; }
+
+    public Aluno(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+        DataCriacao = DateTime.Now;
+        DataAtualizacao = DateTime.Now;
+    }
+}
